9dc01a98d1910b63552fc3d4b6237245ad91ad4d
RusstheGOAT <138267171+RusstheGOAT@users.noreply.github.com>
2024-06-12 15:24:31 +0800

    Update script.js

--- Ramen/static/js/script.js
+++ Ramen/static/js/script.js
@@ -6,7 +6,15 @@ function getRandomRamen() {
                 document.getElementById('random-ramen-result').innerText = data.error;
             } else {
                 document.getElementById('random-ramen-result').innerText = 
-                    `店名: ${data.name}, 地址: ${data.address}, 營業時間: ${data.hours}, 價格範圍: ${data.price_range}`;
+                    `店名: ${data.name}, 城市: ${data.city}, 地址: ${data.address || 'N/A'}, 營業時間: ${data.hours || 'N/A'}, 價格範圍: ${data.price_range || 'N/A'}`;
             }
+        });
+}
+
+function getCurrentTime() {
+    fetch('/current_time')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('current-time-result').innerText = data.current_time;
         });
 }
